3b845b2beec3d93f2277389ee806ba268d38dd3f
KhalilHH <65465578+KhalilHH@users.noreply.github.com>
2020-05-28 05:39:47 +0200

    added numbers

---
+++ number.py
@@ -0,0 +1,20 @@
+print(32+int('33'))
+print(20+10)
+print(23-22)
+print(5*5)
+print(3/2)
+print(3**10)
+
+num1 = 10
+num2 = 20
+num3 = num1 + num2
+print(num3)
+num4=60
+num5=50.5
+num6=num4-num5
+print(num6)
+num7=30
+num8=80
+#num8+=1
+num8=num2+1
+print(num8)
---
+++ srings.py
@@ -0,0 +1,7 @@
+fname = 'ibrahem'
+lname = 'HASSAN'
+full_name = 'ibrahemhassan'
+print(full_name.replace('hassan','hasaballah'))
+
+b = 'A' *66
+print(b)
